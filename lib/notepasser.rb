1e5ea113489ee16b556b1756ef32fd18d5d5510d
Ian Gawronski <iangawronski@gmail.com>
2015-01-31 12:08:10 -0500

    added a create/post message controller

--- lib/notepasser.rb
+++ lib/notepasser.rb
@@ -18,6 +18,25 @@ module Notepasser
 end
 
 module Notepasser::Controllers
+  class MessageCreateController < R "/messages"
+    def post
+      new_message = Notepasser::Models::Message.new
+      [:message_id, :message_text, :sender_id, :recipient_id].each do |p|
+        new_message[p] = @input[p]
+      end
+      new_message.save
+      @status = 201
+      {:message => "Your #{message_id} message was created.", :code => 201, :post => new_message}.to_json
+    end
+
+    def get
+      page = @input['page'].to_i || 1
+      start = (page - 1) * 20
+      finish = (page * 20) - 1
+      Notepasser::Models::Message.where(:id => [start .. finish]).to_json
+    end
+  end
+
   class MessageContorller < R '/messages/(\d+)'
     def get(message_id)
       message = Notepasser::Models::Message.find(message_id)
@@ -36,6 +55,10 @@ module Notepasser::Controllers
       "404 - message not found, unable to delete message"
     end
   end
+end
+
+
+########### TODO: CREATE USER CONTROL AND CLIENT SIDE
 
 #get message, delete message, create/post message, create new user, delete user, message read/unread
 #Keep thinking may need more than just the above...
